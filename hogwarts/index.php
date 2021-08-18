c24f9e1d2fb724ae0ba2700c5e5260e814cf6b1b
pikaybh <pikabh@naver.com>
2021-08-19 00:34:23 +0900

    제발 잘 좀 돼라

--- hogwarts/index.php
+++ hogwarts/index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AI 관상 | 호그와트 기숙사 테스트</title>
-    <link rel="stylesheet" href="style.css">
-    <script defer src="main.js"></script>
+    <link rel="stylesheet" href="../style.css">
+    <script defer src="../main.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"></script>
 </head>
@@ -111,7 +111,7 @@
     </article>
 
     <!--footer-->
-    <?php require_once('/footer.php'); ?>
+    <?php require_once('../footer.php'); ?>
 
 </body>
 
