bd9ad093de8ab629714f1923961c0e3daa60debd
pikaybh <pikabh@naver.com>
2021-08-18 18:38:57 +0900

    margin 0px

--- Hogwarts.php
+++ Hogwarts.php
@@ -5,15 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AI 관상 | 호구와트 기숙사 테스트</title>
-    <link rel="stylesheet" href="/style.css">
-    <script defer src="/main.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script defer src="main.js"></script>
 </head>
 <body>
 <!--navbar-->
-<?php include_once('/navbar.php'); ?>
+<?php include_once('navbar.php'); ?>
 
 <!--footer-->
-<?php require_once('/footer.php'); ?>
+<?php require_once('footer.php'); ?>
 
 </body>
 </html>
