0a201f4646ba4f04e9ab25730885dcda498e5b92
pikaybh <pikabh@naver.com>
2021-09-18 16:14:05 +0900

    backSpace

--- header.php
+++ header.php
@@ -8,5 +8,11 @@
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap" rel="stylesheet">
 
 <header>
-    <span class="material-icons history-back" onclick="history.back()">arrow_back_ios</span>
+    <?php
+        if (isset($_SESSION["useruid"])) {
+            echo "<span class='material-icons history-back' onclick='history.back()'>arrow_back_ios</span>";
+        } else{
+            echo "<a class='logo_text' href='/'><span class='material-icons history-back'>arrow_back_ios 더 많은 컨텐츠</span></a>";
+        }
+    ?>
 </header>
