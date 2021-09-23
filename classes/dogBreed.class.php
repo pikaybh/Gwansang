92e9bcbbf08f3b218eabc5918accc94fe670e355
pikaybh <pikabh@naver.com>
2021-09-23 16:45:57 +0900

    switch

--- classes/dogBreed.class.php
+++ classes/dogBreed.class.php
@@ -5,7 +5,7 @@
         public $test = "강아지 품종";
         public $title = "<h1>강아지 품종</h1>";
         public $subtitle = "<h2>AI가 알려주는 우리 댕댕이 족보!</h2>";
-        public $switch = "<link rel='stylesheet' href='../css/switch.false.css'>";
+        public $switch = "<link rel='stylesheet' href='../css/switch.pet.css'>";
         public $switchName = "";
         public $script = "<script defer src='../js/aiTests/dogBreed.js' type='text/javascript'></script>";
         public $detects = "";//<script defer src='../js/dogFaceDetect.js' type='text/javascript'></script>";
